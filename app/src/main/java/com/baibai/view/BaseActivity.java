fcd12c150b28de5a929b9327a96116587609b615
will.peng <545512533@qq.com>
2016-06-07 06:45:51 +0800

    add volley library add pullToRefresh library

--- app/src/main/java/com/baibai/view/BaseActivity.java
+++ app/src/main/java/com/baibai/view/BaseActivity.java
@@ -3,19 +3,27 @@ package com.baibai.view;
 import android.app.Activity;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 /**
- * @Comments : TODO(用一句话描述该文件做什么)
  * @author will
+ * @Comments : TODO(用一句话描述该文件做什么)
  * @CreateDate : 2016年6月1日 上午9:17:10
  * @ModifiedBy : will
  * @ModifiedDate: 2016年6月1日 上午9:17:10
  * @Modified: TODO(用一句话描述该文件做什么)
  */
-public class BaseActivity extends Activity implements View.OnClickListener{
-	private static final String TAG = "baibai_BaseActivity";
+public class BaseActivity extends Activity implements View.OnClickListener {
+    private static final String TAG = "baibai_BaseActivity";
 
-	@Override
-	public void onClick(View v) {
+    @Override
+    public void onClick(View v) {
 
-	}
+    }
+
+    public RequestQueue getBaseRequest() {
+        return Volley.newRequestQueue(this);
+    }
 }
