734014c2bc11853f1e21970579e2727062526911
will.peng <545512533@qq.com>
2016-06-20 22:13:20 +0800

    add pay add userinfo modify add setting page

--- app/src/main/java/com/baibai/view/PayActivity.java
+++ app/src/main/java/com/baibai/view/PayActivity.java
@@ -1,13 +1,76 @@
 package com.baibai.view;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import com.baibai.R;
+
 /**
- * @Comments : TODO(用一句话描述该文件做什么) 
  * @author will
+ * @Comments : TODO(用一句话描述该文件做什么)
  * @CreateDate : 2016年6月2日 上午11:00:26
  * @ModifiedBy : will
  * @ModifiedDate: 2016年6月2日 上午11:00:26
- * @Modified:
- * TODO(用一句话描述该文件做什么) 
+ * @Modified: TODO(用一句话描述该文件做什么)
  */
-public class PayActivity extends BaseActivity {
-	private static final String TAG = "baibai_PayActivity";
+public class PayActivity extends BaseActivity implements CompoundButton.OnCheckedChangeListener {
+    private static final String TAG = "baibai_PayActivity";
+
+    private TextView mOrderMoney, mOrderId;
+    private CheckBox mCbAliapay, mCbWechatPay, mCbQQPay;
+    private Button mPayConfirm;
+
+    public static final String INTENT_EXTRA_ORDER_ID = "intent_extra_order_id";
+    public static final String INTENT_EXTRA_ORDER_NUM = "intent_extra_order_num";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay);
+
+        initView();
+    }
+
+    private void initView() {
+        mOrderId = (TextView) findViewById(R.id.pay_tv_order_id);
+        mOrderMoney = (TextView) findViewById(R.id.pay_tv_order_money);
+
+        mCbAliapay = (CheckBox) findViewById(R.id.pay_cb_alipay);
+        mCbWechatPay = (CheckBox) findViewById(R.id.pay_cb_wechat);
+        mCbQQPay = (CheckBox) findViewById(R.id.pay_cb_qqwallet);
+
+        mPayConfirm = (Button) findViewById(R.id.pay_btn_confirm);
+        mPayConfirm.setOnClickListener(this);
+
+        mCbAliapay.setOnCheckedChangeListener(this);
+        mCbWechatPay.setOnCheckedChangeListener(this);
+        mCbQQPay.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()) {
+            case R.id.pay_cb_alipay:
+                if (isChecked) {
+                    mCbQQPay.setChecked(false);
+                    mCbWechatPay.setChecked(false);
+                }
+                break;
+            case R.id.pay_cb_wechat:
+                if (isChecked) {
+                    mCbQQPay.setChecked(false);
+                    mCbAliapay.setChecked(false);
+                }
+                break;
+            case R.id.pay_cb_qqwallet:
+                if (isChecked) {
+                    mCbAliapay.setChecked(false);
+                    mCbWechatPay.setChecked(false);
+                }
+                break;
+        }
+    }
 }
---
+++ app/src/main/java/com/baibai/view/SettingActivity.java
@@ -0,0 +1,29 @@
+package com.baibai.view;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.baibai.R;
+
+public class SettingActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        initView();
+    }
+
+    private void initView() {
+        setRightBtnVisible(View.GONE);
+        setLeftBtnOnclick();
+        setLeftText("");
+        setCenterText(R.string.title_activity_setting);
+    }
+
+}
